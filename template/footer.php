4a2af5b6ca5f4ca41b363353da53ac9fceb98d23
Seje27878 <Seje27878@edu.ucl.dk>
2021-06-11 01:07:16 +0200

    footer + css

---
+++ template/footer.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/footer.css">
+    <title></title>
+
+  </head>
+  <body>
+    <footer class="footer">
+      <table>
+        <div>
+        <tr class="text">
+
+           <th>Åbningstider<hr>Varehuset:
+             Alle ugens dage kl. 10-20
+             Bistro: Alle ugens dage kl. 10-20
+             Restaurant: Lukket
+             SMÅLAND: Lukket</th>
+
+           <th>Kundeservice<hr>
+            Telefon: 70 15 09 09
+            Mandag - fredag: 9 - 20
+            Lørdag - søndag: 9 - 18</th>
+         </tr>
+       </div>
+      </table>
+    </footer>
+  </body>
+</html>
