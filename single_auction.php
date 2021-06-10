0a0b5a5f42b2141f1811e2d47fab928976fa540c
celu29462 <73479501+celu29462@users.noreply.github.com>
2021-06-10 21:05:54 +0200

    hotfix

--- single_auction.php
+++ single_auction.php
@@ -38,9 +38,9 @@ include('template/footer.php');
           ?>
           <h2><?php echo "Auktion slutter om " . $days_remaining . " dage, " . $hours_remaining . " timer og " . $minutes_remaining . " minuter" ?></h2>
           <h3>Slut dato: <?php echo $row['expiration'] ?></h3>
-          <form class="" action="single_auction.php?aucid=<?php echo $_GET['aucid'] ?>" method="post">
-            <input type="number" name="bid_amount" value="" placeholder="Give bud her">
-            <button type="submit" name="bid_button">Giv bud</button>
+        <form class="" action="single_auction.php?aucid=<?php echo $_GET['aucid'] ?>" method="post">
+          <input type="number" name="bid_amount" value="" placeholder="Give bud her">
+          <button type="submit" name="bid_button">Giv bud</button>
           <?php
           $username = $_SESSION['username'];
           $sql_query = "SELECT id, username FROM users WHERE username='$username'";
